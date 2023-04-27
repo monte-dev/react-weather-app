b7602377ab3278580ac7e29e65ce536187f53899
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-04-27 11:27:00 +0100

    add error handling, fix component rendenring conditions

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,33 +1,45 @@
 import PickCity from '../PickCity/PickCity';
+import ErrorBox from '../ErrorBox/ErrorBox';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
 const WeatherBox = (props) => {
 	const API_KEY = '61181c3bd64ee97f041e27773f39b4bf';
-	const [weather, setWeather] = useState({});
+	const [weather, setWeather] = useState('');
+	const [pending, setPending] = useState(false);
+	const [dataError, setDataError] = useState(false);
+
 	const handleCityChange = useCallback((cityName) => {
-		console.log(cityName);
+		setPending(true);
+		setDataError(false);
 		fetch(
 			`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				const weatherData = {
-					city: data.name,
-					temp: data.main.temp,
-					icon: data.weather[0].icon,
-					description: data.weather[0].main,
-				};
-				setWeather(weatherData);
-			});
+		).then((res) => {
+			if (res.status === 200) {
+				return res.json().then((data) => {
+					const weatherData = {
+						city: data.name,
+						temp: data.main.temp,
+						icon: data.weather[0].icon,
+						description: data.weather[0].main,
+					};
+					setPending(false);
+					setWeather(weatherData);
+				});
+			} else {
+				setWeather('');
+				setPending(false);
+				setDataError(true);
+			}
+		});
 	}, []);
-	console.log(weather);
+
 	return (
 		<section>
 			<PickCity handleCityChange={handleCityChange} />
-			<WeatherSummary {...weather} />
-			<Loader />
+			{weather && !pending && <WeatherSummary {...weather} />}
+			{pending && <Loader />}
+			{dataError && <ErrorBox children="There is no such city" />}
 		</section>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
