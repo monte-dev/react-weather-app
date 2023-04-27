85f29740c478932539c0dfa511eb21c2fc469c56
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-04-27 01:49:33 +0100

    Connect to API and display weather data by user input

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -4,17 +4,28 @@ import styles from './PickCity.module.scss';
 
 import { useState } from 'react';
 
-const PickCity = () => {
-  const [city, setCity] = useState('');
+const PickCity = ({ handleCityChange }) => {
+	const [city, setCity] = useState('');
 
-  return (
-    <form className={styles.pickCityForm}> 
-      <label>
-        <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
-      </label>
-      <Button>Search</Button>
-    </form>
-  );
+	return (
+		<form
+			className={styles.pickCityForm}
+			onSubmit={(e) => {
+				e.preventDefault();
+				handleCityChange(city);
+				setCity('');
+			}}
+		>
+			<label>
+				<TextInput
+					placeholder="Enter city name...."
+					value={city}
+					onChange={(e) => setCity(e.target.value)}
+				/>
+			</label>
+			<Button>Search</Button>
+		</form>
+	);
 };
 
 export default PickCity;
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,35 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-
-const WeatherBox = props => {
-  return (
-    <section>
-      <PickCity />
-      <WeatherSummary />
-      <Loader />
-    </section>
-  )
+import { useCallback, useState } from 'react';
+const WeatherBox = (props) => {
+	const API_KEY = '61181c3bd64ee97f041e27773f39b4bf';
+	const [weather, setWeather] = useState({});
+	const handleCityChange = useCallback((cityName) => {
+		console.log(cityName);
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				const weatherData = {
+					city: data.name,
+					temp: data.main.temp,
+					icon: data.weather[0].icon,
+					description: data.weather[0].main,
+				};
+				setWeather(weatherData);
+			});
+	}, []);
+	console.log(weather);
+	return (
+		<section>
+			<PickCity handleCityChange={handleCityChange} />
+			<WeatherSummary {...weather} />
+			<Loader />
+		</section>
+	);
 };
 
 export default WeatherBox;
